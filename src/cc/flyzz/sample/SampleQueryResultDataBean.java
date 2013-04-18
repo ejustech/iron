f0f292cd23bdbfa0044779de703c417bc8347726
赵峰 <zhaof@ejustech.com>
2013-04-18 21:53:37 +0800

    Sample做成。 检索条件画面，跳转到检索结果画面，结果画面的内容作为明细行显示。 cc.flyzz.sample

---
+++ src/cc/flyzz/sample/SampleQueryResultDataBean.java
@@ -0,0 +1,136 @@
+package cc.flyzz.sample;
+
+public class SampleQueryResultDataBean {
+
+	/** id property */
+	private String id;
+
+	/** lastName property */
+	private String lastName;
+
+	/** iPhone property */
+	private String phoneNumber;
+
+	/** school property */
+	private String school;
+
+	/** fullName property */
+	private String fullName;
+
+	/** firstName property */
+	private String firstName;
+
+	/**
+	 * Returns the id.
+	 * 
+	 * @return String
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * Set the id.
+	 * 
+	 * @param id
+	 *            The id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * Returns the lastName.
+	 * 
+	 * @return String
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * Set the lastName.
+	 * 
+	 * @param lastName
+	 *            The lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * Returns the iPhone.
+	 * 
+	 * @return String
+	 */
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	/**
+	 * Set the iPhone.
+	 * 
+	 * @param iPhone
+	 *            The iPhone to set
+	 */
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	/**
+	 * Returns the school.
+	 * 
+	 * @return String
+	 */
+	public String getSchool() {
+		return school;
+	}
+
+	/**
+	 * Set the school.
+	 * 
+	 * @param school
+	 *            The school to set
+	 */
+	public void setSchool(String school) {
+		this.school = school;
+	}
+
+	/**
+	 * Returns the fullName.
+	 * 
+	 * @return String
+	 */
+	public String getFullName() {
+		return fullName;
+	}
+
+	/**
+	 * Set the fullName.
+	 * 
+	 * @param fullName
+	 *            The fullName to set
+	 */
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	/**
+	 * Returns the firstName.
+	 * 
+	 * @return String
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * Set the firstName.
+	 * 
+	 * @param firstName
+	 *            The firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+}
