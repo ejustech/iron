ee1295f42b02757e4b79635756c3d80498156830
kik <kik@ikik.pw>
2013-04-29 20:15:49 +0800

    常量定义，为信息录入画面的验证做准备

--- src/com/ejustech/iron/common/Constant.java
+++ src/com/ejustech/iron/common/Constant.java
@@ -6,187 +6,371 @@ public class Constant {
 	 */
 	public static final int INPUT_BY_NEW_ROWS = 30;
 	/**
-	 * 年月日
+	 * 信息输入画面 登陆用SQL文 参数顺序 年月日
 	 */
 	public static final int T_IRON_INFO_COL_YearMonthDay = 1;
 	/**
-	 * 期号
+	 * 信息输入画面 登陆用SQL文 参数顺序 期号
 	 */
 	public static final int T_IRON_INFO_COL_QiHao = 2;
 	/**
-	 * 炉次
+	 * 信息输入画面 登陆用SQL文 参数顺序 炉次
 	 */
 	public static final int T_IRON_INFO_COL_LuCi = 3;
 	/**
-	 * 军品
+	 * 信息输入画面 登陆用SQL文 参数顺序 军品
 	 */
 	public static final int T_IRON_INFO_COL_JunPinFlg = 4;
 	/**
-	 * 规格
+	 * 信息输入画面 登陆用SQL文 参数顺序 规格
 	 */
 	public static final int T_IRON_INFO_COL_GuiGe = 5;
 	/**
-	 * 生产炉号
+	 * 信息输入画面 登陆用SQL文 参数顺序 生产炉号
 	 */
 	public static final int T_IRON_INFO_COL_ShengChanLuHao = 6;
 	/**
-	 * 反应器号
+	 * 信息输入画面 登陆用SQL文 参数顺序 反应器号
 	 */
 	public static final int T_IRON_INFO_COL_FanYingQiHao = 7;
 	/**
-	 * 使用次数
+	 * 信息输入画面 登陆用SQL文 参数顺序 使用次数
 	 */
 	public static final int T_IRON_INFO_COL_ShiYongCiShu = 8;
 	/**
-	 * Mg（t）
+	 * 信息输入画面 登陆用SQL文 参数顺序 Mg（t）
 	 */
 	public static final int T_IRON_INFO_COL_Mg = 9;
 	/**
-	 * TiCl4
+	 * 信息输入画面 登陆用SQL文 参数顺序 TiCl4
 	 */
 	public static final int T_IRON_INFO_COL_TiCl4 = 10;
 	/**
-	 * 毛重
+	 * 信息输入画面 登陆用SQL文 参数顺序 毛重
 	 */
 	public static final int T_IRON_INFO_COL_MaoZhong = 11;
 	/**
-	 * 净重
+	 * 信息输入画面 登陆用SQL文 参数顺序 净重
 	 */
 	public static final int T_IRON_INFO_COL_JingZhong = 12;
 	/**
-	 * 成品率
+	 * 信息输入画面 登陆用SQL文 参数顺序 成品率
 	 */
 	public static final int T_IRON_INFO_COL_ChengPinLv = 13;
 	/**
-	 * Fe
+	 * 信息输入画面 登陆用SQL文 参数顺序 Fe
 	 */
 	public static final int T_IRON_INFO_COL_YuanSuFe = 14;
 	/**
-	 * Si
+	 * 信息输入画面 登陆用SQL文 参数顺序 Si
 	 */
 	public static final int T_IRON_INFO_COL_YuanSuSi = 15;
 	/**
-	 * Cl
+	 * 信息输入画面 登陆用SQL文 参数顺序 Cl
 	 */
 	public static final int T_IRON_INFO_COL_YuanSuCl = 16;
 	/**
-	 * C
+	 * 信息输入画面 登陆用SQL文 参数顺序 C
 	 */
 	public static final int T_IRON_INFO_COL_YuanSuC = 17;
 	/**
-	 * N
+	 * 信息输入画面 登陆用SQL文 参数顺序 N
 	 */
 	public static final int T_IRON_INFO_COL_YuanSuN = 18;
 	/**
-	 * O
+	 * 信息输入画面 登陆用SQL文 参数顺序 O
 	 */
 	public static final int T_IRON_INFO_COL_YuanSuO = 19;
 	/**
-	 * H
+	 * 信息输入画面 登陆用SQL文 参数顺序 H
 	 */
 	public static final int T_IRON_INFO_COL_YuanSuH = 20;
 	/**
-	 * Mn
+	 * 信息输入画面 登陆用SQL文 参数顺序 Mn
 	 */
 	public static final int T_IRON_INFO_COL_YuanSuMn = 21;
 	/**
-	 * HB
+	 * 信息输入画面 登陆用SQL文 参数顺序 HB
 	 */
 	public static final int T_IRON_INFO_COL_YuanSuHb = 22;
 	/**
-	 * 等级(含锰)
+	 * 信息输入画面 登陆用SQL文 参数顺序 等级(含锰)
 	 */
 	public static final int T_IRON_INFO_COL_DengJiHanMeng = 23;
 	/**
-	 * 考核等级(除锰)
+	 * 信息输入画面 登陆用SQL文 参数顺序 考核等级(除锰)
 	 */
 	public static final int T_IRON_INFO_COL_KaoHeDengJiChuMeng = 24;
 	/**
-	 * 工艺调整
+	 * 信息输入画面 登陆用SQL文 参数顺序 工艺调整
 	 */
 	public static final int T_IRON_INFO_COL_GongYiTiaoZheng = 25;
 	/**
-	 * 工艺试验
+	 * 信息输入画面 登陆用SQL文 参数顺序 工艺试验
 	 */
 	public static final int T_IRON_INFO_COL_GongYiShiYan = 26;
 	/**
-	 * 底皮Kg
+	 * 信息输入画面 登陆用SQL文 参数顺序 底皮Kg
 	 */
 	public static final int T_IRON_INFO_COL_DiPiKg = 27;
 	/**
-	 * 上帽Kg
+	 * 信息输入画面 登陆用SQL文 参数顺序 上帽Kg
 	 */
 	public static final int T_IRON_INFO_COL_ShangMaoKg = 28;
 	/**
-	 * 爬壁Kg
+	 * 信息输入画面 登陆用SQL文 参数顺序 爬壁Kg
 	 */
 	public static final int T_IRON_INFO_COL_PaBiKg = 29;
 	/**
-	 * 废底皮Kg
+	 * 信息输入画面 登陆用SQL文 参数顺序 废底皮Kg
 	 */
 	public static final int T_IRON_INFO_COL_FeiDiPiKg = 30;
 	/**
-	 * 废上帽Kg
+	 * 信息输入画面 登陆用SQL文 参数顺序 废上帽Kg
 	 */
 	public static final int T_IRON_INFO_COL_FeiShangMaoKg = 31;
 	/**
-	 * 废爬壁Kg
+	 * 信息输入画面 登陆用SQL文 参数顺序 废爬壁Kg
 	 */
 	public static final int T_IRON_INFO_COL_FeiPaBiKg = 32;
 	/**
-	 * 废钛粉Kg
+	 * 信息输入画面 登陆用SQL文 参数顺序 废钛粉Kg
 	 */
 	public static final int T_IRON_INFO_COL_FeiTaiFenKg = 33;
 	/**
-	 * 磁选Kg
+	 * 信息输入画面 登陆用SQL文 参数顺序 磁选Kg
 	 */
 	public static final int T_IRON_INFO_COL_CiYuanKg = 34;
 	/**
-	 * 手选废料Kg
+	 * 信息输入画面 登陆用SQL文 参数顺序 手选废料Kg
 	 */
 	public static final int T_IRON_INFO_COL_ShouXuanFeiLiaoKg = 35;
 	/**
-	 * 损耗Kg
+	 * 信息输入画面 登陆用SQL文 参数顺序 损耗Kg
 	 */
 	public static final int T_IRON_INFO_COL_SunHaoKg = 36;
 	/**
-	 * 总排镁量Kg
+	 * 信息输入画面 登陆用SQL文 参数顺序 总排镁量Kg
 	 */
 	public static final int T_IRON_INFO_COL_ZongPaiMeiLiangKg = 37;
 	/**
-	 * 出炉真空度Pa
+	 * 信息输入画面 登陆用SQL文 参数顺序 出炉真空度Pa
 	 */
 	public static final int T_IRON_INFO_COL_ChuLuZhenKongDu = 38;
 	/**
-	 * 还原最高温度℃
+	 * 信息输入画面 登陆用SQL文 参数顺序 还原最高温度℃
 	 */
 	public static final int T_IRON_INFO_COL_YuanZuiGaoWenDu = 39;
 	/**
-	 * 蒸馏高恒A点最高℃
+	 * 信息输入画面 登陆用SQL文 参数顺序 蒸馏高恒A点最高℃
 	 */
 	public static final int T_IRON_INFO_COL_ZhengLiuGaoHengDian = 40;
 	/**
-	 * 转蒸馏
+	 * 信息输入画面 登陆用SQL文 参数顺序 转蒸馏
 	 */
 	public static final int T_IRON_INFO_COL_ZhuanZhengLiu = 41;
 	/**
-	 * 加首次
+	 * 信息输入画面 登陆用SQL文 参数顺序 加首次
 	 */
 	public static final int T_IRON_INFO_COL_JiaShouCi = 42;
 	/**
-	 * 加末次
+	 * 信息输入画面 登陆用SQL文 参数顺序 加末次
 	 */
 	public static final int T_IRON_INFO_COL_JiaMoCi = 43;
 	/**
-	 * 通道
+	 * 信息输入画面 登陆用SQL文 参数顺序 通道
 	 */
 	public static final int T_IRON_INFO_COL_TongDao = 44;
 	/**
-	 * 生产故障
+	 * 信息输入画面 登陆用SQL文 参数顺序 生产故障
 	 */
 	public static final int T_IRON_INFO_COL_ShengChanGuZhang = 45;
 	/**
-	 * 备注说明
+	 * 信息输入画面 登陆用SQL文 参数顺序 备注说明
 	 */
 	public static final int T_IRON_INFO_COL_BeiZhuShuoMing = 46;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 年月日
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_YearMonthDay = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 期号
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_QiHao = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 炉次
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_LuCi = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 军品
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_JunPinFlg = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 规格
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_GuiGe = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 生产炉号
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_ShengChanLuHao = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 反应器号
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_FanYingQiHao = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 使用次数
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_ShiYongCiShu = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 Mg（t）
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_Mg = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 TiCl4
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_TiCl4 = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 毛重
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_MaoZhong = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 净重
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_JingZhong = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 成品率
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_ChengPinLv = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 Fe
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_YuanSuFe = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 Si
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_YuanSuSi = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 Cl
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_YuanSuCl = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 C
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_YuanSuC = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 N
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_YuanSuN = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 O
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_YuanSuO = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 H
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_YuanSuH = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 Mn
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_YuanSuMn = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 HB
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_YuanSuHb = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 等级(含锰)
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_DengJiHanMeng = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 考核等级(除锰)
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_KaoHeDengJiChuMeng = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 工艺调整
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_GongYiTiaoZheng = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 工艺试验
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_GongYiShiYan = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 底皮Kg
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_DiPiKg = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 上帽Kg
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_ShangMaoKg = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 爬壁Kg
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_PaBiKg = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 废底皮Kg
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_FeiDiPiKg = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 废上帽Kg
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_FeiShangMaoKg = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 废爬壁Kg
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_FeiPaBiKg = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 废钛粉Kg
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_FeiTaiFenKg = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 磁选Kg
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_CiYuanKg = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 手选废料Kg
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_ShouXuanFeiLiaoKg = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 损耗Kg
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_SunHaoKg = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 总排镁量Kg
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_ZongPaiMeiLiangKg = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 出炉真空度Pa
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_ChuLuZhenKongDu = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 还原最高温度℃
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_YuanZuiGaoWenDu = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 蒸馏高恒A点最高℃
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_ZhengLiuGaoHengDian = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 转蒸馏
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_ZhuanZhengLiu = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 加首次
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_JiaShouCi = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 加末次
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_JiaMoCi = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 通道
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_TongDao = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 生产故障
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_ShengChanGuZhang = 20;
+	/**
+	 * 信息输入画面 文本框输入值 最大长度验证 备注说明
+	 */
+	public static final int LENGTH_INPUT_BY_NEW_CHECK_BeiZhuShuoMing = 20;
 }
